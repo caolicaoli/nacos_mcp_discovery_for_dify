f1fde69809d01a13fae33db3b975fb644e1a68e6
caolicaoli <122708729+caolicaoli@users.noreply.github.com>
2025-08-21 08:58:01 +0800

    Update mcp_post.py

--- endpoints/mcp_post.py
+++ endpoints/mcp_post.py
@@ -58,7 +58,9 @@ class McpPostEndpoint(Endpoint):
                     "serverInfo": { "name": "Nacos MCP Dify Plugin", "version": "1.0.0" }
                 }
             }
-        
+        elif method == "notifications/initialized":
+            response = {}
+            
         if is_debug(settings):
             response_str = json.dumps(response, ensure_ascii=False, indent=2)
             logger.info(f"response is {response_str}")
